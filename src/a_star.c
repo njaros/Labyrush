57057118c8cdf79cb081f18a05bb0e5e5f8adf75
jajalecapouet <nicolas.jarose@gmail.com>
2022-04-23 20:14:27 +0200

    supp des commentaires nuls

--- src/a_star.c
+++ src/a_star.c
@@ -59,7 +59,6 @@ void	closeddelete(t_list **closed, int x, int y)
 	t_list	*temp;
 	noeud	*current;
 
-	//fprintf(stderr, "\ndel en cours \n");
 	temp = NULL;
 	first = *closed;
 	current = (*closed)->content;
@@ -81,7 +80,6 @@ void	closeddelete(t_list **closed, int x, int y)
 		free(*closed);
 		*closed = first;
 	}
-	//fprintf(stderr, "\nfin del\n");
 }
 
 int	closed_val(t_list *closed, int x, int y)
@@ -140,18 +138,15 @@ void	check_noeud(int x, int y, t_list *open, t_list *closed, int **heuristiques,
 	noeud	*current;
 
 	current = open->content;
-	//fprintf(stderr, "x = %d, y = %d, cx = %d, cy = %d | map[y][x] = %c", x, y, current->x, current->y, map[y][x]);
 	if (map[y][x] != '#' && map[y][x] != '?')
 	{
 		val_closed = closed_val(closed, x, y);
-		// fprintf(stderr, "valeur de val_closed et valeur noeud teste : %d et %d <- pos {%d, %d}\n", val_closed, heuristiques[y][x] + open->n->cout + 1, x, y);
 		if (heuristiques[y][x] + current->cout + 1 < val_closed || val_closed == -1)
 		{
 			new = crea_noeud(x, y, current, heuristiques);
 			ft_lstinsert_croissant(&open, ft_lstnew(new));
 			if (val_closed != -1)
 				closeddelete(&closed, x, y);
-		//    fprintf(stderr, "%d %d\n", open->n->cout, open->next->n->cout);
 		}
 	}
 }
@@ -185,7 +180,6 @@ int	a_star(char **map, int row, int col, int xa, int ya, int xb, int yb)
 	heuristiques[row] = NULL;
 	open = NULL;
 	closed = NULL;
-	// creation des heuristiques des noeuds
 	while (j < row)
 	{
 		heuristiques[j] = malloc(sizeof(int) * col);
@@ -200,44 +194,31 @@ int	a_star(char **map, int row, int col, int xa, int ya, int xb, int yb)
 		i = 0;
 		j++;
 	}
-	//fprintf(stderr, "heuristiques creees\n");
 	depart = crea_noeud(xa, ya, NULL, heuristiques);
 	ft_lstadd_back(&open, ft_lstnew(depart));
 	current = open->content;
-	//creation open et closed de A*
 	while (open && heuristiques[current->y][current->x] != 0)
 	{
 		i = current->x;
 		j = current->y;
-		// Recherche et creation des noeuds voisins + creation open et correction closed
 		check_noeud(i + 1, j, open, closed, heuristiques, map);
 		check_noeud(i - 1, j, open, closed, heuristiques, map);
 		check_noeud(i, j + 1, open, closed, heuristiques, map);
 		check_noeud(i, j - 1, open, closed, heuristiques, map);
-		// fin de recherche et creation de noeuds voisins + open et delclosed
-		ft_lstadd_back(&closed, ft_lsttake(&open)); //noeud lu va dans closed
+		ft_lstadd_back(&closed, ft_lsttake(&open));
 		if (open)
 			current = open->content;
-		//fprintf(stderr, "heuristique nouvel open : %ld\n", heuristiques[open->n->y][open->n->x]);
 	}
-	//fprintf(stderr, "listes faites\n");
 	i = xb;
 	j = yb;
 	ft_lstadd_back(&closed, ft_lsttake(&open));
-	// open et closed creees
-	// Creation du chemin a suivre
 	while (i != xa || j != ya)
 	{
 		current = search_parent(closed, i, j);
-	//    fprintf(stderr, "coord parent trouve : {%d, %d}\n", next_node->x, next_node->y);
-	//	fill_path(path, next_node->x - i, next_node->y - j);
 		timer++;
 		i = current->x;
 		j = current->y;
 	}
-	//fprintf(stderr, "\npathing memorise \n");
-	// chemin a suivre cree
-	// free des noeuds et open closed
 	t_list  *temp;
 	while (open)
 	{
